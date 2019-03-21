fe539195d21f23ae721bfc2dc59c199155af4152
lisbethmorrison <l.morrison@pgr.reading.ac.uk>
2019-03-21 12:14:31 +0000

    Edit code to remove filters at end of growth rate calc

--- Butterfly_scripts/2_calc_growth_rates.R
+++ Butterfly_scripts/2_calc_growth_rates.R
@@ -48,8 +48,8 @@ for (j in site.list){
 		# convert to growth rate
 		if(length(temp.table[temp.table$YEAR==i,"SINDEX"]) + length(temp.table[temp.table$YEAR==(i-1),"SINDEX"]) < 2){		# if there is less than two years of data
 			gr <- c(gr,NA) # gr gets NA
-		# } else if (temp.table[temp.table$YEAR==(i-1), "SINDEX"] == 1){ ## if previous year == 1 (i.e. no count made), gr = NA to avoid synchrony being calculated on string of zeros
-		#   gr <- c(gr, NA) # gr gets NA
+		} else if (temp.table[temp.table$YEAR==(i-1), "SINDEX"] == 1){ ## if previous year == 1 (i.e. no count made), gr = NA to avoid synchrony being calculated on string of zeros
+		  gr <- c(gr, NA) # gr gets NA
 		} else {
 			gr <- c(gr, (log(temp.table[temp.table$YEAR==i,"SINDEX"]) - log(temp.table[temp.table$YEAR==(i-1),"SINDEX"]))) # else gr gets the log growth rate calculation
 		}
@@ -92,15 +92,15 @@ final_data <- rbind(final_data, new_butterfly_final)   #  all species data
 # final_data <- merge(final_data, final_data3, by=c("site", "name"))
 # length(unique(final_data$site)) ## 768 sites
 
-# this filter has been removed
-### drop sites with >50% zero counts ###
-good_year_data <- zero_count_data[zero_count_data$good_years>5,] ## dataframe with species & site combo with more than 5 years of non-zero counts
-final_data$rec_id <- paste(final_data$name, final_data$site, sep="_")
-good_year_data$rec_id <- paste(good_year_data$SPECIES, good_year_data$SITE, sep="_")
+# # this filter has been removed
+# ### drop sites with >50% zero counts ###
+# good_year_data <- zero_count_data[zero_count_data$good_years>5,] ## dataframe with species & site combo with more than 5 years of non-zero counts
+# final_data$rec_id <- paste(final_data$name, final_data$site, sep="_")
+# good_year_data$rec_id <- paste(good_year_data$SPECIES, good_year_data$SITE, sep="_")
+# 
+# final_data <- final_data[final_data$rec_id%in%good_year_data$rec_id,]
+# ## 59 species and 1055 sites
 
-final_data <- final_data[final_data$rec_id%in%good_year_data$rec_id,]
-## 59 species and 1055 sites
-
-write.csv(final_data, file = "../Data/Butterfly_sync_data/final_data_all_spp_zeros.csv", row.names = FALSE)
+write.csv(final_data, file = "../Data/Butterfly_sync_data/final_data_all_spp_no_zeros2.csv", row.names = FALSE)
 length(unique(final_data$name)) # 59 species
 length(unique(final_data$site)) # 768 sites
